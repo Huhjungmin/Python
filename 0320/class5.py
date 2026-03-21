fc74354f9bf8ef470d8a5edf88bc51e9ef2a4c9e
Huhjungmin <140683323+Huhjungmin@users.noreply.github.com>
2026-03-21 18:48:22 +0900

    Delete 0320 directory

--- 0320/class5.py
+++
@@ -1,6 +0,0 @@
-mylist = [1, 2, 3, 4, 5]
-
-mylist[0] = 0
-mylist[-1] = 9
-
-print(mylist)
--- 0320/ex2.py
+++
@@ -1,8 +0,0 @@
-price = int(input("가격을입력하시오: "))
-card = (input("카드종류를입력하시오: "))
-
-if price > 20000 and card == "python":
-    print("배송료가없습니다.")
-
-else:
-    print("배송료는3000원입니다.")
